3f3dd7e74a894e66e8325f4695a706139ec5d214
Neeraj Nandwana <neerajnandwana@gmail.com>
2014-12-11 16:58:48 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp'),
-    qunit = require('gulp-qunit');
+    qunit = require('gulp-qunit'),
+    coveralls = require('./index.js');
 
 gulp.task('default', function() {
     return gulp.src('./test.html')
         .pipe(qunit()); 
 });
+
+gulp.task('coveralls', function() {
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls());
+});
+
+gulp.task('default', ['default', 'coveralls'])
