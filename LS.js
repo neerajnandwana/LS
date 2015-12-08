0c5e78d741f1e1c12a276895f81b941152cf62ca
Neeraj Nandwana <neerajnandwana@gmail.com>
2015-12-08 19:18:54 +0530

    minor optimisation
    Minor optimi

--- LS.js
+++ LS.js
@@ -1,20 +1,27 @@
 (function(window, undefined){
 	'use strict';
-	
+
 	var uid = 0,
 		slice = Array.prototype.slice,
 		store = window.localStorage,
+		falsyValues = {
+			'0': 		false,
+			'': 		false,
+			'null': 	false,
+			'undefined':false,
+			'false': 	false
+		},
 		listeners = {
 			create: [],
 			update: [],
 			remove: [],
 			clear: []
 		};
-    
+
 	function empty(val){
 		return val === undefined || val === null;
 	}
-	
+
 	function isFunc(fn){
 		return typeof fn === 'function';
 	}
@@ -22,23 +29,23 @@
 	function isStringFloat(n){
 		return n.indexOf('.') > -1 ? true:false;
 	}
-	
+
 	function getFnId(type, fn){
 		return fn['___' + type + '_id'];
 	}
-	
+
 	function getFnScope(type, fn){
 		return fn['___' + type + '_scope'];
 	}
-	
+
 	function setFnId(type, fn, val){
 		fn['___' + type + '_id'] = val;
 	}
-	
+
 	function setFnScope(type, fn, val){
 		fn['___' + type + '_scope'] = val;
 	}
-	
+
 	/* trigger all the listeners registered with given type */
 	function trigger(type){
 		if(!listeners[type]) return;
@@ -48,45 +55,36 @@
 			};
 		listeners[type].forEach(callFn);
 	}
-	
+
 	/* return unique id */
-	function nextId(){		
+	function nextId(){
 		return 'LS-ID-' + (++uid);
 	}
-	
+
 	function LS(){}
-	
+
 	var LSProto = LS.prototype;
-	
+
 	LSProto.get = function(key){
 		if(empty(key)) return;
 		return store.getItem(key);
 	}
-	
+
 	LSProto.getJson = function(key){
 		if(empty(key)) return;
 		return JSON.parse(store.getItem(key));
 	}
-	
+
 	LSProto.getNumber = function(key){
-		if(empty(key)) return;		
+		if(empty(key)) return;
 		var val = store.getItem(key);
 		return isStringFloat(val) ? parseFloat(val) : parseInt(val, 10);
 	}
-	
+
 	LSProto.getBoolean = function(key){
 		if(empty(key)) return;
-		var val = store.getItem(key) || '',
-			//false value list
-			boolVal = {
-				'0': 		false,
-				'': 		false,
-				'null': 	false,
-				'undefined':false,
-				'false': 	false
-			};
-		
-		return boolVal[val] === false ? false: true;
+		var val = store.getItem(key) || '';
+		return falsyValues[val] === false ? false: true;
 	}
 
 	LSProto.set = function(key, value){
@@ -95,7 +93,7 @@
 		store.setItem(key, value);
 		trigger(empty(oldVal) ? 'create': 'update', key, value, oldVal);
 	}
-	
+
 	LSProto.setJson = function(key, value){
 		if(empty(key) || empty(value)) return;
 		var oldVal = LSProto.get(key);
@@ -113,7 +111,7 @@
 		store.clear();
 		trigger('clear');
 	}
-	
+
 	/* register a listener for type. 
 	   It will return the function which can be used for un-registering the listener. */
 	LSProto.on = function(type, fn, scope){
@@ -123,7 +121,7 @@
 		listeners[type].push(fn);
 		return fn;
 	}
-	
+
 	/* remove the listener for given type */
 	LSProto.un = function(type, fn){
 		if(!listeners[type] || !isFunc(fn)) return;
@@ -136,15 +134,15 @@
 			}
 		}
 	}
-	
+
 	/* remove all the listeners for given type */
 	LSProto.removeAllEventListeners = function(type){
 		if(!listeners[type]) return;
 		listeners[type] = [];
 	}
 
-	window.LS = window.LS || new LS();    
-	
+	window.LS = window.LS || new LS();
+
 	/* Register as a named AMD module */
 	if (typeof define === 'function' && define.amd) {
 		define('LS', [], function() {
